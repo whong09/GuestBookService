0b2d92a1277afec3e2f53343c751c4ce9b5e1b60
Wayne Hong <whong@csod.com>
2018-04-18 18:38:01 -0700

    Guest Book Service

---
+++ GuestBookService/Models/GuestBookEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuestBookService.Models
+{
+    public class GuestBookEntry
+    {
+        public string GuestName { get; set; }
+        public string GuestComment { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
